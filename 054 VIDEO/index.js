4d99b7c5678b2f9d3f235611c2f234f5a8654f86
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-06-17 17:03:08 +0500

    Video 54

---
+++ 054 VIDEO/index.js
@@ -0,0 +1,16 @@
+// alert("Hello World")
+console.log("Hey i am console");
+var a = prompt("Enter your age");
+console.log("Your age is " + a);
+var b = confirm("are you sure");
+if (b){
+    console.log("yes");
+    
+}
+else {
+    console.log("no");
+    
+}
+document.title= "asad"
+document.body.style.backgroundColor = "red";  
+
