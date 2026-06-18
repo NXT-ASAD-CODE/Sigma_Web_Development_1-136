3b53f7e9eb1981bd8243ed051cf0863813aa296e
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-06-18 17:22:14 +0500

    Video 55

---
+++ 055 VIDEO/index.js
@@ -0,0 +1,32 @@
+console.log("Hello World");
+var a = 5;
+var b = 10 ;
+var c = a+b;
+console.log(c);
+var d = "asad";
+console.log(typeof d);
+var _a = "Asad" // valid name of variable
+// var 55a = "Asad" // Unvalid name of variable
+const ab = 6;
+// ab = ab + 1 not allowed
+let name = "Ali"
+let num = 1
+{
+    num = num + 1
+    console.log(num);
+    
+}
+console.log(num);
+let x = "asad"
+let y = 22;
+let z = 3.55
+const p = true;
+let w = undefined;
+let r = null;
+let q = {
+    uname : "asad",
+    "S password":12345
+}
+q.education = "matric"
+
+
