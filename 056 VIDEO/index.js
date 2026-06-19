01af33e6c1f6363b6bc0b34fb19f62e7c94893cf
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-06-19 20:43:57 +0500

    Video 56

---
+++ 056 VIDEO/index.js
@@ -0,0 +1,75 @@
+console.log("Hello world");
+let age = 45;
+grace = 2
+num1 = 2;
+num2 = 2;
+b = 10
+c = 10
+a = num1 += num2
+console.log(a);
+// Operators
+console.log(age + grace);
+console.log(age - grace);
+console.log(age / grace);
+console.log(age * grace);
+console.log(age ** grace);
+console.log(age % grace);
+// if else conditions
+if (age > 18) {
+    console.log("you can drive");
+
+}
+else {
+    console.log("You cannot Drive");
+}
+if (num1 == num2) {
+    console.log(true);
+
+}
+else {
+    console.log(false);
+
+}
+if (num1 != num2) {
+    console.log(true);
+
+}
+else {
+    console.log(false);
+
+}
+if (num1 != num2) {
+    console.log(true);
+
+}
+else {
+    console.log(false);
+
+}
+// comparision operator
+console.log("3" == 3);
+console.log("3" === 3);
+
+// logical operator
+console.log(num1 != num2 && c != b);
+console.log(num1 != num2 || c != b);
+console.log(num1 != num2 != c != b);
+
+// Multi line comment 
+/* i am a multi line comment */
+if (a == b) {
+    console.log(true);
+
+}
+else if (a == c) {
+    console.log(true);
+
+}
+else {
+    console.log(false);
+
+}
+console.log(a == b ? true : false);
+
+
+
