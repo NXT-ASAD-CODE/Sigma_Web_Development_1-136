6e5a5e7a921cb1c738b38c45b47a8d6602d35513
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-06-26 12:03:47 +0500

    Video 60

---
+++ 060 VIDEO/index.js
@@ -0,0 +1,23 @@
+let a = "asad";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+let firstName = "Asad";
+let lastName = "Iftikhar"
+console.log("My first name is " + firstName + " and my last name is " + lastName ); // not a good method
+console.log(`My first name is ${firstName} and my last name is ${lastName}`); // good method
+console.log(firstName.toUpperCase());
+console.log(firstName.toLowerCase());
+console.log(firstName.slice(1,2));
+console.log(firstName.replace("d","s"));
+console.log(firstName.charAt(2));
+console.log(firstName.startsWith("A"));
+console.log(firstName.endsWith("d"));
+
+
+
+
+
+
+
