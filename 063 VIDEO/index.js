59eccf0fb982c0fad846670415bcd3762e1e5207
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-07-01 22:21:17 +0500

    Video 63

--- 063 VIDEO/index.js
+++ 063 VIDEO/index.js
@@ -9,6 +9,18 @@ console.log(arr.pop());
 console.log(arr);
 console.log(arr.push("Asad"));
 console.log(arr);
+console.log(arr.shift());
+console.log(arr.unshift("hello"));
+console.log(arr);
+console.log(delete arr[3]);
+console.log(arr);
+let a1 = [1,2,3]
+let a2 = [4,5,6]
+let a3 = [7,8,9]
+console.log(a1.concat(a2,a3));
+
+
+
 
 
 
