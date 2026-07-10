3c924dab4a5e547a3cab0f1c0c80e4429a08522e
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-07-10 23:25:10 +0500

    Video 67

--- 067 VIDEO/index.js
+++ 067 VIDEO/index.js
@@ -1,2 +1,9 @@
 // index.js
 console.log("this is from index.js");
+console.log(document.body.childNodes[0]); //text
+console.log(document.body.childNodes[1]);
+console.log(document.body.childNodes[1].childNodes);
+console.log(document.body.childNodes[1].firstChild);
+console.log(document.body.childNodes[1].lastChild);
+console.log(document.body.childNodes[1].lastChild.style);
+
