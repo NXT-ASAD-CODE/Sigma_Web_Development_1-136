30d18fb9b19edf3aa9b4cbddae1cf40cb538074e
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-07-13 21:14:26 +0500

    Video 67

--- 067 VIDEO/index.js
+++ 067 VIDEO/index.js
@@ -7,4 +7,10 @@ console.log(document.body.childNodes[1].firstChild);
 console.log(document.body.childNodes[1].lastChild);
 console.log(document.body.childNodes[1].lastChild.style);
 console.log(document.body.childNodes[1].lastChild.style.backgroundColor = "red");
+console.log(document.body.firstElementChild.children[0]);
+console.log(document.body.firstElementChild.children[1].nextElementSibling);
+console.log(document.body.firstElementChild.children[1].previousElementSibling);
+console.log(document.body.firstElementChild.children[1].previousSibling);
+
+
 
