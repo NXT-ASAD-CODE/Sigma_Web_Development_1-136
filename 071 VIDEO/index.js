a8966a610dfcabfe5b9f443bfcdf6c868441587e
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-07-16 16:36:03 +0500

    Video 71

--- 071 VIDEO/index.js
+++ 071 VIDEO/index.js
@@ -9,8 +9,12 @@ let textcontent = document.getElementsByClassName("box").textContent
 let innertext = document.getElementsByClassName("box").innerText = "Hello world"
 let hasattribute = document.getElementsByClassName("box1").hasAttribute("style") // tells is this element style attribute is present or not.
 let getattribute = document.getElementsByClassName("box1").getAttribute("style") // it gives the attribute value 
-let setattribute = document.getElementsByClassName("box1").setAttribute("style","display:flex") // it sets the attribute which is after given by the comma (,)
+let setattribute = document.getElementsByClassName("box1").setAttribute("style", "display:flex") // it sets the attribute which is after given by the comma (,)
 let attribute = document.getElementsByClassName("box1").attributes
-let removeattribute = document.getElementsByClassName("box1").removeAttributes("style")
-
-
+let removeattribute = document.getElementsByClassName("box1").removeAttributes("style") // it removes attribute which is given in the bracket
+let designmode = "on" // now when this is on we can edit any thing in the website
+let dataset = document.getElementsByClassName("box").dataset
+let remove = document.querySelector(".box").remove()
+let classlist = document.querySelector(".box").classList
+let toggle = document.querySelector(".box").classList.toggle("blue") // if there is the class blue it in it will remove if the blue class is not present it will add
+let classname = document.querySelector(".box").className
