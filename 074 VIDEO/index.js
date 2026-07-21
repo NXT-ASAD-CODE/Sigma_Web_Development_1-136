01ac66f93ba64dbce362c6dc4f07a4f6e69b1e62
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-07-21 20:34:44 +0500

    Video 74

--- 074 VIDEO/index.js
+++ 074 VIDEO/index.js
@@ -1,4 +1,14 @@
 let btn = document.getElementById("change")
-btn.addEventListener("click",()=>{
-    document.querySelector(".box").innerText = "I am changed suii"
+// btn.addEventListener("dblclick",()=>{
+//     document.querySelector(".box").innerText = "I am changed suii"
+// })
+// btn.addEventListener("click",()=>{
+//     document.querySelector(".box").innerText = "I am changed suii"
+// })
+// btn.addEventListener("contextmenu",()=>{
+//     document.querySelector(".box").innerText = "I am changed suii"
+// })
+btn.addEventListener("keydown",(e)=>{
+    console.log(e);
+    
 })
