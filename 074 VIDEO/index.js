f0e5a95932ad0314b6997ad3bcdad7d328113c45
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-07-23 19:52:26 +0500

    Video 74

--- 074 VIDEO/index.js
+++ 074 VIDEO/index.js
@@ -12,3 +12,11 @@ btn.addEventListener("keydown",(e)=>{
     console.log(e.key);
     
 })
+
+// random color genrator 
+       function getRandomColor() {
+            let value1 = Math.ceil( 0 + Math.random() *255 ) 
+            let value2 = Math.ceil( 0 + Math.random() *255 )
+            let value3 = Math.ceil( 0 + Math.random() *255 )
+            return `rgb(${value1} , ${value2} , ${value3})`
+        }
